a5491f88fa8f2701fece9bbb9d6b3efae09675c0
joaquingalang <galangjoaquin@gmail.com>
2025-10-18 12:18:32 +0800

    Update: no outline

--- src/components/UpdateInput.js
+++ src/components/UpdateInput.js
@@ -5,7 +5,7 @@ function UpdateInput({ type = "text", placeholder, value, onChange }) {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className="col-span-8 bg-transparent border-b-2 border-dotted border-[#FF97E0] w-[296px] font-poppins font-extralight text-white text-lg"
+      className="col-span-8 bg-transparent border-b-2 border-dotted border-[#FF97E0] w-[296px] font-poppins font-extralight text-white text-lg outline-none"
     />
   );
 }
